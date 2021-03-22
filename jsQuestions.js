8a81e9914d44fa7a987fb473c4edb0d24400cf89
jade <jadealvares@hotmail.co.uk>
2021-03-22 12:38:58 +0000

    default parameters and classes(with static methods and extending from main class)

--- jsQuestions.js
+++ jsQuestions.js
@@ -130,3 +130,72 @@ const { nameee, address: { fullAddress : {doorNumber : theDoorNumber} } } = user
 console.log(nameee, theDoorNumber); //John 22
 
 // ----------------------------------------------------------------------------------------------------------------------------------
+//Function default parameters
+
+// const calculatePay = (salary, bonus=0) => {
+//   return salary + bonus;
+// }
+
+const calculatePay = (salary, bonus = {
+  teamBonus: 0,
+  employeeBonus: 0
+}) => {
+  return salary + bonus.teamBonus + bonus.employeeBonus;
+}
+
+console.log(calculatePay(38000, {teamBonus: 5000, employeeBonus: 1000} ));
+
+// ----------------------------------------------------------------------------------------------------------------------------------
+//classes
+//constructor - a  method that gets invoked whenever you call the class.  Constructors only get invoked once: when we create the instance of our class
+
+class Animal {
+  constructor(name, age) {
+    console.log(`${name} is an animal and was created`);
+    this.name = name; //properties
+    this.age = age;
+  }
+
+  static iAmAStaticMethod() {
+    console.log('static method inside animal class');
+  }
+
+  eat = () => {
+    console.log(`${this.name} is eating`);
+  }
+
+  logAge = () =>  {
+    console.log(`${this.name} is ${this.age}`);
+  }
+}
+
+//instantiate
+Animal.iAmAStaticMethod();
+const bob = new Animal('Bob', 2); //how we create new instances/objects from this class
+bob.eat();
+bob.logAge();
+
+//inheritance - we have a common class and extend properties and behaviours from those classes
+
+class Dog extends Animal {
+  constructor(name, age, breed) {
+    super(name, age); //the call to the super constructor which takes care of assigning name and age
+    this.breed = breed;
+  }
+
+  logBreed = () => {
+    console.log(`${this.name} is a ${this.breed}`);
+  }
+
+  logDogAge = () => {
+    super.logAge();
+  }
+}
+
+const rey = new Dog('Rey', 4, 'Husky');
+rey.logBreed();
+///rey.logDogAge(); //doesnt work i dont understand as it should ????????????
+
+
+
+// ----------------------------------------------------------------------------------------------------------------------------------
