5cad54983c9ea4281043cabbdf03d74711285084
jade <jadealvares@hotmail.co.uk>
2021-03-23 19:48:48 +0000

    async await functions with fetch and error handling

--- amigosCode/asyncAwait.js
+++ amigosCode/asyncAwait.js
@@ -1,15 +1,67 @@
+const fetch = require("node-fetch");
+
 async function logName(name) {
-  console.log(name);
+  console.log(1, name);
   //we can yield promises using await this function returns a promise
   return name;
 }
 
 logName('tom').then(res => {
-  console.log('result from async function = ' + res);
+  console.log(3, 'result from async function = ' + res);
 });
 //you get:
 // tom
 // result from async function = tom
 
-//if didnt have asunc on line 1 you would get tom and an error
-//cos cant return the promise (line 4 return name is a promise), if its not an async function.  line 8 fails.
+//if didnt have async on line 3 you would get tom and an error
+//cos cant return the promise (line 6 return name is a promise), if its not an async function.  line 8 fails.
+
+async function logName2(name) {
+  console.log(2, name);
+  const transformName = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(name.toUpperCase())
+    }, 5000);
+
+  });
+  const result = await transformName;
+  console.log(4, result);
+  return name;
+}
+
+logName2('antonio').then(res => {
+  console.log(5, 'result from async2 function = ' + res);
+})
+
+//if you place async before a functions, you can await promises
+//await waits until the promise returns a result back
+
+//you get:
+// 1 tom
+// 2 antonio
+// 3 result from async function = tom
+// 4 ANTONIO
+// 5 result from async2 function = antonio
+
+//------------------------------------------------------------------------------------------------------------------------------
+
+const getRandomUsers = async (n) => {
+  const fetchRandomUsers = await fetch(`https://randomuser.me/api/?results=${n}`);
+  const data = await fetchRandomUsers.json();
+  data.results.forEach((user) => {
+      const { gender, email } = user;
+      console.log(`${gender}, ${email}`);
+    });
+  return data;
+};
+
+getRandomUsers(2);
+
+// getRandomUsers(10).then((randomUsers) => {
+//   randomUsers.results.forEach((user) => {
+//     const { gender, email } = user;
+//     console.log(`${gender}, ${email}`);
+//   });
+// }).catch((error) => {
+//   console.log(error);
+// })
