0525edbd5cc6fa7f54dbfda8337fa4c008db0d33
jade <jadealvares@hotmail.co.uk>
2021-03-29 20:46:17 +0100

    added one happy path test

--- funFunctions/unitTesting/__tests__/order-total-test.js
+++ funFunctions/unitTesting/__tests__/order-total-test.js
@@ -1,5 +1,15 @@
 const orderTotal = require('../order-total');
 
+//const order = items: []
+
 it('wewe', () => {
   expect(1).toBe(1);
+})
+
+it("quantity", () => {
+  expect(orderTotal({
+    items: [
+      { name: 'Dragon candy', price: 2, quantity: 3 }
+    ]
+  })).toBe(6);
 })
