18072b5266efebb9f61dd57e3fdd1bac0ac99218
jade <jadealvares@hotmail.co.uk>
2021-05-26 08:35:09 +0100

    satisifed 6 conditions for nextBigNumWithSameDigits file and had a go at kata3 question with not much luck

--- codewars/Kata3/lastDigit.js
+++ codewars/Kata3/lastDigit.js
@@ -1,44 +1,3 @@
-// const lastDigit = (arr) => {
-//   if (arr.length === 0) {
-//     return 1;
-//   }
-//   if (arr.length === 1) {
-//     return arr[0];
-//   } else if (arr.length === 2) {
-//     return Math.pow(arr[1], arr[0])
-//   } else {
-//     return thePower(arr);
-//   }
-// }
-//
-// const thePower = (arr) => {
-//   for (var i = 0; i < arr.length-1; i++) {
-//
-//     const remainderOfArr = arr.reverse().splice(-1, 1); // take 1 last element of arr
-//     const newVal = Math.pow(arr[i], arr[i+1]);
-//     console.log('arr[i]', arr[i]);
-//     console.log('arr[i]+1', arr[i+1]);
-//     console.log('remainderOfArr', remainderOfArr); //[3]
-//     console.log('newVal', newVal); //16
-//
-//     if (remainderOfArr.length === 1) {
-//       console.log('remainderOfArr[0]', remainderOfArr[0]);
-//       const bigNum = Math.pow(remainderOfArr[0], newVal);
-//       const resArr = String(bigNum).split('').map((num) => {return Number(num)})
-//       return resArr[resArr.length-1];
-//
-//     } else if (remainderOfArr.length === 0) {
-//       const chars = newVal.split('');
-//       console.log(chars[chars.length-1]);
-//       return chars[chars.length-1];
-//
-//     } else {
-//     thePower([...remainderOfArr, newVal]);
-//     }
-//
-//   }
-// }
-
 
 const lastDigit = (arr) => {
   console.log(1)
@@ -50,6 +9,8 @@ const lastDigit = (arr) => {
   if (arr.length === 1) {
     console.log(3);
     return arr[0];
+  } else if (arr.every((elem) => elem === 0) && arr.length > 2) {
+    return 0;
   } else if (arr.length === 2) {
     console.log(4)
     return Math.pow(arr[0], arr[1])
@@ -60,9 +21,11 @@ const lastDigit = (arr) => {
 }
 
 const thePower = (arr) => {
+  const backwards = arr.reverse();
   for (var i = 0; i < arr.length-1; i++) {
 
-    const remainderOfArr = arr.reverse().splice(-1, 1); // take 1 last element of arr
+    const remainderOfArr = backwards.splice(-1, 1); // take 1 last element of arr
+    // const newValLastDigit = lasttDigitt(arr[i+1], arr[i]);
     const newVal = Math.pow(arr[i], arr[i+1]);
     console.log('arr[i]', arr[i]);
     console.log('arr[i]+1', arr[i+1]);
@@ -71,7 +34,9 @@ const thePower = (arr) => {
 
     if (remainderOfArr.length === 1) {
       console.log('remainderOfArr[0]', remainderOfArr[0]);
-      const bigNum = Math.pow(remainderOfArr[0], newVal);
+      // const bigNum = Math.pow(remainderOfArr[0], newVal);
+
+      const bigNum = lasttDigitt(remainderOfArr[0], newVal);
       console.log('bigNum', bigNum);
       const resArr = String(bigNum).split('').map((num) => {return Number(num)})
       return resArr[resArr.length-1];
@@ -92,5 +57,73 @@ const thePower = (arr) => {
 //console.log(thePower([3, 4, 2]));
 //console.log(thePower([4, 3, 1, 2])); //64
 
-//console.log(lastDigit([3, 4, 2])); // 3^(4^2) = 3^16 = 43046721 --> 1
-console.log(lastDigit([3, 4, 5])); // 3^ 1024 -->1
+console.log(lastDigit([3, 4, 2])); // 3^(4^2) = 3^16 = 43046721 --> 1
+//console.log(lastDigit([3, 4, 5])); // 3^ 1024 -->1
+//console.log(lastDigit([4, 3, 6])); //4
+
+//console.log(lastDigit([7, 6, 21])); //1
+
+
+
+// Function to find b % a
+function Modulo(lastNum, secondToLastNum)
+{
+
+   // Initialize result
+   let result = 0;
+
+   // calculating mod of b with a to make
+   // b like 0 <= b < a
+   for (let i = 0; i < secondToLastNum.length; i++)
+       result = (result * 10 + secondToLastNum[i] - '0') % lastNum;
+
+   return result; // return modulo
+}
+
+// function to find last digit of a^b
+function lasttDigitt(firstNum, secondNum) {
+
+    const a = firstNum.toString();
+    const b = secondNum.toString();
+
+   let len_a = a.length;
+   let len_b = b.length;
+   console.log('***', a.length);
+
+   // if a and b both are 0
+   if (len_a == 1 && len_b == 1 &&
+               b[0] == '0' && a[0] == '0')
+       return 1;
+
+   // if exponent is 0
+   if (len_b == 1 && b[0] == '0')
+       return 1;
+
+   // if base is 0
+   if (len_a == 1 && a[0] == '0')
+       return 0;
+
+   // if exponent is divisible by 4 that
+   // means last digit will be pow(a, 4)
+   // % 10. if exponent is not divisible
+   // by 4 that means last digit will be
+   // pow(a, b%4) % 10
+   const exp = (Modulo(4, b) == 0) ? 4 :
+                           Modulo(4, b);
+
+                           console.log('exp', exp);
+
+   // Find last digit in 'a' and compute
+   // its exponent
+   console.log(len_a);
+   const res = Math.pow(a[len_a - 1] - '0', exp);
+   console.log('res', res);
+
+   // Return last digit of result
+   return res % 10;
+}
+
+// Driver program to run test case
+// let a = "117";
+// let b = "3";
+// document.write(LastDigit(a, b));
--- codewars/Kata4/nextBigNumWithSameDigits.js
+++ codewars/Kata4/nextBigNumWithSameDigits.js
@@ -1,35 +1,60 @@
 const nextBigger = (n) => {
 
-  const originalSize = n.toString().length
+  const originalSize = n.toString().length;
+
   if (originalSize === 1) {
     return -1;
+
   } else {
     let arrOfStr =  ('' +n).split('');
+
 
     for (let i = arrOfStr.length-1; i > -1; i--) {
       console.log('p', parseInt(arrOfStr[i]) < parseInt(arrOfStr[i-1]));
       console.log(parseInt(arrOfStr[i]));
       console.log(parseInt(arrOfStr[i-1]));
+
       if (parseInt(arrOfStr[i]) < parseInt(arrOfStr[i-1])) {
         return -1;
-      } else if (parseInt(arrOfStr[i]) === parseInt(arrOfStr[i-1])) {
-        for (let j = arrOfStr.length-1; j > -1; j--) {
-          console.log('****', parseInt(arrOfStr[i-j]));
-          if (parseInt(arrOfStr[i]) < parseInt(arrOfStr[i-j])) {
-            arrOfStr.splice(arrOfStr.length -j, 0, arrOfStr[i])
-            arrOfStr.pop();
-          }
-        }
-      } else {
-        arrOfStr.splice(arrOfStr.length -2, 0, arrOfStr[i])
-        arrOfStr.pop();
 
-        if (originalSize <= 3) {
+      // } else if (parseInt(arrOfStr[i]) === parseInt(arrOfStr[i-1])) {
+      //   continue;
+      //   // for (let j = arrOfStr.length-1; j > -1; j--) {
+      //   //   console.log('****', parseInt(arrOfStr[i-j]));
+      //   //   if (parseInt(arrOfStr[i]) < parseInt(arrOfStr[i-j])) {
+      //   //     arrOfStr.splice(arrOfStr.length -j, 0, arrOfStr[i])
+      //   //     arrOfStr.pop();
+      //   //   }
+      //   // }
+      //
+       }
+       else {
+          console.log('here');
+        // arrOfStr.splice(arrOfStr.length -2, 0, arrOfStr[i])
+        arrOfStr.splice(-2, 0, arrOfStr[i])
+        console.log(arrOfStr);
+        arrOfStr.pop();
+        console.log(arrOfStr);
+        //it should come here and be mutated and ready to compare the next 2 integers??! instead keeps iterating over same thing!
+
+
+        // if (originalSize <= 3) {
+        // console.log('p', parseInt(arrOfStr[i]) < parseInt(arrOfStr[i-1]));
+        // console.log(parseInt(arrOfStr[i]));
+        // console.log(parseInt(arrOfStr[i-1]));
+        if (parseInt(arrOfStr[i]) < parseInt(arrOfStr[i-1])) {
+          console.log('here2', parseInt(arrOfStr.join('')));
           return parseInt(arrOfStr.join(''));
         } else {
-          //count++;
-          arrOfStr = [...nextBigger(parseInt(arrOfStr.join('')))];
-          return parseInt(arrOfStr.join(''));
+          const lastNumRemovedFromMain = arrOfStr.splice(arrOfStr.length -1, 1);
+          console.log('lasNumRemovedFromMain', arrOfStr);
+          console.log('what goes in', parseInt(arrOfStr.join('')));
+          arrOfStr = [nextBigger(parseInt(arrOfStr.join('')))];
+          console.log('arrOfStr[0]', arrOfStr[0]);
+          console.log('lastNumRemovedFromMainStr', lastNumRemovedFromMain.toString());
+          const theEnd = `${arrOfStr[0]}${lastNumRemovedFromMain.toString()}`
+          console.log('theEnd', theEnd);
+          return parseInt(theEnd);
         }
       }
     }
@@ -37,8 +62,8 @@ const nextBigger = (n) => {
 }
 
 
-//console.log(nextBigger(12)); // 21
-// console.log(nextBigger(513)); // 531
-// console.log(nextBigger(2017)); // 2071
-//console.log(nextBigger(414)); // 441
-console.log(nextBigger(144)); // 414
+console.log(nextBigger(12)); // 21
+ console.log(nextBigger(513)); // 531
+console.log(nextBigger(2017)); // 2071
+ console.log(nextBigger(414)); // 441
+ console.log(nextBigger(144)); // 414
